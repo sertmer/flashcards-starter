dc90ec3c4c6091fb4386366e78ea26325fed1d86
sertmer <scottertmer@gmail.com>
2019-09-30 19:41:27 -0600

    Add start func to game.js

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,9 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +14,15 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    // starts it all
+    // instantiate cards
+    // push cards into deck (instantiates deck from array of cards??)
+    // creates new round using deck (??)
+    // invokes printMessage in the CLI
+    // invokes printQuestion to kick off helper functions that allow interaction via the CLI
   }
 }
 
