45dae48f86090886cd7e6392877e5ccc6588fedd
sertmer <scottertmer@gmail.com>
2019-10-01 20:48:58 -0600

    Begin building Deck-test, starting with beforeEach()

---
+++ test/Deck-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+  let deck;
+  let card1;
+  let card2;
+  let card3;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3])
+  });
+});
